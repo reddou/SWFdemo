55a0e0959e7ec723e817f89a90d4aa66a8b983d3
“sdhong” <1723785802@qq.com>
2022-02-14 11:07:52 +0800

    刚刚

---
+++ CProject/CProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CProjectApp.swift
+//  CProject
+//
+//  Created by 证联 on 2022/2/14.
+//
+
+import SwiftUI
+
+@main
+struct CProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
